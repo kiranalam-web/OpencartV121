ede95ab49cb2b62d6de85ff2898f1adb3efdcc6e
SAI KIRAN ALAM <saikiranalam@gmail.com>
2025-05-15 21:54:23 +0530

    Update BaseClass.java

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -79,7 +79,7 @@ public Properties p;
 			default: System.out.println("No matching browser"); return;
 			}
 			
-			driver=new RemoteWebDriver(new URL(" http://192.168.26.177:4444/wd/hub"),capabilities);
+			driver=new RemoteWebDriver(new URL(" http://localhost:4444/wd/hub"),capabilities);
 		}
 		
 				
